5006576a27aa17f7871b22aaf0a349cc7689971d
BiraBalaZ <erick.m.anjos@gmail.com>
2024-07-12 15:44:48 -0300

    My First Commit

---
+++ automacaoChrome.py
@@ -0,0 +1,21 @@
+# pip install selenium
+# pip install webdriver-manager
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriver
+from selenium.webdriver.chrome.service import Service
+
+servico = Service(ChromeDriver().install())
+navegador = webdriver.Chrome(service=servico)
+
+page = 'https://pages.hashtagtreinamentos.com/arquivo-python-1jGh7kZSxQLoznA_GgwnWa8f7LEl3kKCZ?origemurl=hashtag_yt_org_planilhapyt_8AMNaVt0z_M'
+navegador.get(page)
+
+# Escrevendo E-Mail no formulário
+navegador.find_element('xpath', '/html/body/div[2]/div[1]/section/div[2]/div/form/div[1]/div/div/div/input').send_keys('teste@gmail.com')
+
+# Clicando no botão de enviar no formulário
+navegador.find_element('xpath', '/html/body/div[2]/div[1]/section/div[2]/div/form/button').click()
+
+# Fechando a aba
+navegador.quit()
